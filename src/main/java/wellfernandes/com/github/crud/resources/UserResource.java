66a3b7f56ea8eceb17b2c419f17ac8532e3900d2
Welliton Fernandes Leal <wellfernandes@me.com>
2022-05-27 20:24:05 -0300

    entity user and rest working

---
+++ src/main/java/wellfernandes/com/github/crud/resources/UserResource.java
@@ -0,0 +1,30 @@
+package wellfernandes.com.github.crud.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import wellfernandes.com.github.crud.domain.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@RequestMapping(method = RequestMethod.GET) // or @GetMapping
+	public ResponseEntity<List<User>> findAll() {
+
+		// mock
+		User maria = new User("1", "Maria Brown", "maria@gmail.com");
+		User alex = new User("2", "Alex Green", "alex@gmail.com");
+
+		List<User> list = new ArrayList<>();
+		list.addAll(Arrays.asList(maria, alex));
+
+		return ResponseEntity.ok().body(list);
+	}
+}
