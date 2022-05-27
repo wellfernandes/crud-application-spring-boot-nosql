532c7bd4a700c1d23ad5aa93f0eaf0da5e8455d7
Welliton Fernandes Leal <wellfernandes@me.com>
2022-05-27 15:23:35 -0300

    project created

---
+++ src/test/java/wellfernandes/com/github/crud/CrudApplicationSpringBootNosqlApplicationTests.java
@@ -0,0 +1,13 @@
+package wellfernandes.com.github.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationSpringBootNosqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
