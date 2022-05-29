eaedd175a76dac74da450363538fb298c528fcaa
Welliton Fernandes Leal <wellfernandes@me.com>
2022-05-29 15:22:11 -0300

    connecting to mongodb with repository and service

---
+++ src/main/java/wellfernandes/com/github/crud/services/UserService.java
@@ -0,0 +1,20 @@
+package wellfernandes.com.github.crud.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import wellfernandes.com.github.crud.domain.User;
+import wellfernandes.com.github.crud.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+}
