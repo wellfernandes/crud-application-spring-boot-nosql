dec8547498e509d25cfd25654a75c1056768cd7a
Welliton Fernandes Leal <wellfernandes@me.com>
2022-05-30 13:00:57 -0300

    getting a user by id

--- src/main/java/wellfernandes/com/github/crud/services/UserService.java
+++ src/main/java/wellfernandes/com/github/crud/services/UserService.java
@@ -1,12 +1,14 @@
 package wellfernandes.com.github.crud.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import wellfernandes.com.github.crud.domain.User;
 import wellfernandes.com.github.crud.repository.UserRepository;
+import wellfernandes.com.github.crud.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,11 @@ public class UserService {
 
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> user = userRepository.findById(id);
+
+		return user.orElseThrow(() -> new ObjectNotFoundException("object not found!"));
 	}
 }
---
+++ src/main/java/wellfernandes/com/github/crud/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package wellfernandes.com.github.crud.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
