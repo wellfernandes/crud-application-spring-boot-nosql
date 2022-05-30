32951ce824d880e5f38488e084a219fb8b6567a8
Welliton Fernandes Leal <wellfernandes@me.com>
2022-05-30 11:58:18 -0300

    using the dto pattern to return users

--- src/main/java/wellfernandes/com/github/crud/resources/UserResource.java
+++ src/main/java/wellfernandes/com/github/crud/resources/UserResource.java
@@ -1,6 +1,7 @@
 package wellfernandes.com.github.crud.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import wellfernandes.com.github.crud.domain.User;
+import wellfernandes.com.github.crud.dto.UserDTO;
 import wellfernandes.com.github.crud.services.UserService;
 
 @RestController
@@ -19,8 +21,11 @@ public class UserResource {
 	private UserService userService;
 
 	@RequestMapping(method = RequestMethod.GET) // or @GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
+
 		List<User> list = userService.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+
+		return ResponseEntity.ok().body(listDto);
 	}
 }
