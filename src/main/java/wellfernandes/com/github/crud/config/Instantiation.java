983e32100ddd7b51b8d6dcceee534176aaed8fe7
Welliton Fernandes Leal <wellfernandes@me.com>
2022-05-31 15:57:58 -0300

    adding comments to posts

--- src/main/java/wellfernandes/com/github/crud/config/Instantiation.java
+++ src/main/java/wellfernandes/com/github/crud/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import wellfernandes.com.github.crud.domain.Post;
 import wellfernandes.com.github.crud.domain.User;
 import wellfernandes.com.github.crud.dto.AuthorDTO;
+import wellfernandes.com.github.crud.dto.CommentDTO;
 import wellfernandes.com.github.crud.repository.PostRepository;
 import wellfernandes.com.github.crud.repository.UserRepository;
 
@@ -43,6 +44,13 @@ public class Instantiation implements CommandLineRunner {
 				new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "good morning!!", "I woke up happy today.",
 				new AuthorDTO(maria));
+
+		CommentDTO c1 = new CommentDTO("good trip, bro!", sdf.parse("21/03/2022"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Enjoy!", sdf.parse("22/03/2022"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Have a nice day!", sdf.parse("21/03/2022"), new AuthorDTO(alex));
+
+		post1.getListComments().addAll(Arrays.asList(c1, c2));
+		post2.getListComments().addAll(Arrays.asList(c3));
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
--- src/main/java/wellfernandes/com/github/crud/domain/Post.java
+++ src/main/java/wellfernandes/com/github/crud/domain/Post.java
@@ -1,13 +1,16 @@
 package wellfernandes.com.github.crud.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import wellfernandes.com.github.crud.dto.AuthorDTO;
+import wellfernandes.com.github.crud.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -20,6 +23,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+
+	private List<CommentDTO> listComments = new ArrayList<>();
 
 	public Post() {
 
@@ -72,6 +77,14 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+	
+	public List<CommentDTO> getListComments() {
+		return listComments;
+	}
+
+	public void setListComments(List<CommentDTO> listComments) {
+		this.listComments = listComments;
 	}
 
 	@Override
