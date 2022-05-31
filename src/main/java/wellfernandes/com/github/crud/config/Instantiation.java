e2ecc9959e48a1515e029f4a25d7264ed91e9a33
Welliton Fernandes Leal <wellfernandes@me.com>
2022-05-31 11:34:18 -0300

    creating entity post with nested user

--- src/main/java/wellfernandes/com/github/crud/config/Instantiation.java
+++ src/main/java/wellfernandes/com/github/crud/config/Instantiation.java
@@ -1,12 +1,16 @@
 package wellfernandes.com.github.crud.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import wellfernandes.com.github.crud.domain.Post;
 import wellfernandes.com.github.crud.domain.User;
+import wellfernandes.com.github.crud.repository.PostRepository;
 import wellfernandes.com.github.crud.repository.UserRepository;
 
 @Configuration
@@ -15,15 +19,28 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private PostRepository postRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
 
+		// mock
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "lets travel!", "Im going to Sao Paulo!", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "good morning!!", "I woke up happy today.", maria);
+
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
+
 	}
 }
